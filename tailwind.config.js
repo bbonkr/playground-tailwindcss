4ce013b0466c180480b2921a4b2f6fed9948497e
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-10-05 13:00:33 +0900

    Setup tailwind css #1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        boxShadow: {
+            bottom: '0 0.1rem rgba(0, 0, 0, 0.2)',
+        },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
